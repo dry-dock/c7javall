fc64984ed924c79937defe5fdedb95070bcc02a7
bindushree16 <37651858+bindushree16@users.noreply.github.com>
2018-05-24 12:14:57 +0530

    Update oraclejdk8.sh

--- version/oraclejdk8.sh
+++ version/oraclejdk8.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 echo "================ Installing oracle-java8 ================="
-wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u172-b11/a58eab1ec242421181065cdc37240b08/jre-8u172-linux-x64.rpm"
-sudo yum localinstall -y jre-8u172-linux-x64.rpm
-wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u172-b11/a58eab1ec242421181065cdc37240b08/jdk-8u172-linux-x64.rpm"
-sudo yum localinstall -y jdk-8u172-linux-x64.rpm
+wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u171-b11/512cd62ec5174c3487ac17c61aaa89e8/jre-8u171-linux-x64.rpm"
+sudo yum localinstall -y jre-8u171-linux-x64.rpm
+wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u171-b11/512cd62ec5174c3487ac17c61aaa89e8/jdk-8u171-linux-x64.rpm"
+sudo yum localinstall -y jdk-8u171-linux-x64.rpm
