0213a74aa8b34f7acfd1745e7e2739afaf0ff579
bindushree16 <37651858+bindushree16@users.noreply.github.com>
2018-10-25 11:39:46 +0530

    Update openjdk8.sh

--- version/openjdk8.sh
+++ version/openjdk8.sh
@@ -3,7 +3,7 @@
 echo "================ Installing openjdk8 ================="
 sudo yum install -y java-1.8.0-openjdk-devel
 
-ln -s /usr/java/java-1.8.0-openjdk-1.8.0.191.b12-0.el7_5.x86_64/ /usr/lib/jvm/java-8-openjdk-amd64
+ln -s /usr/lib/jvm/java-1.8.0-openjdk-1.8.0.191.b12-0.el7_5.x86_64/ /usr/lib/jvm/java-8-openjdk-amd64
 
 update-alternatives --install /usr/bin/java java /usr/lib/jvm/java-8-openjdk-amd64/bin/java 1
 update-alternatives --install /usr/bin/java javac /usr/lib/jvm/java-8-openjdk-amd64/bin/javac 1
