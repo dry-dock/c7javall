e4c1f1672b52a650e4d9cf70dc1b9f085a66aabb
bindushree16 <37651858+bindushree16@users.noreply.github.com>
2018-10-25 18:14:13 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -43,8 +43,6 @@ mv tools/ /opt/android-sdk/
 
 ln -fs /opt/android-sdk/tools/bin/sdkmanager /usr/bin
 
-ln -s /usr/java/jdk1.8.0_192-amd64/ /usr/java/java-8-oraclejdk-amd64
-
 export JAVA_HOME=/usr/java/java-8-oraclejdk-amd64
 export PATH=$PATH=/usr/java/java-8-oraclejdk-amd64/bin
 export ANDROID_HOME=/opt/android-sdk
